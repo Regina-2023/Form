4818417e67a3628b1ea9fd836fb63defb4c66ff4
Regina Faritova <faritova_regina@mail.ru>
2023-05-16 00:00:55 +0300

    add files

---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,10 @@
+package org.example;
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    boolean subscription;
+    public FormDate birthday;
+}
